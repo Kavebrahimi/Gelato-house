abc5cf144e5d01e5cd3244062d81d12135655c03
Kave <kaveermak@gmail.com>
2025-12-22 16:31:29 +0330

    changed btn factory and added primary & secondary colors

--- src/components/base/button/index.tsx
+++ src/components/base/button/index.tsx
@@ -4,38 +4,47 @@ import type React from "react";
 
 type ButtonProps = {
   variant: "contained" | "outlined" | "text";
-  color?: "primary" | "secondary"
-
+  color?: "primary" | "secondary",
+  size?: "small" | "medium" | "large"
 } & React.PropsWithChildren & React.HTMLAttributes<HTMLButtonElement>
+
+const variants = {
+  contained: {
+    primary: "bg-primary-500 text-white",
+    secondary: "bg-secondary-500 text-white",
+  },
+  outlined: {
+    primary: "border-2 border-primary-500 text-primary-500",
+    secondary: "border-2 border-secondary-500 text-secondary-500",
+  },
+  text: {
+    primary: "text-gray-900",
+    secondary: "text-gray-900",
+  },
+}
+
+const sizes = {
+  small: "px-2 py-1 text-sm",
+  medium: "px-4 py-4 text-base",
+  large: "px-6 py-6 text-lg"
+}
+
 
 export const Button: React.FC<ButtonProps> = ({
   variant,
   children,
   color = "primary",
+  size = "medium",
   ...props
 }): JSX.Element => {
-  return (()=>{
-    switch (variant) {
-      case "contained":
-        return (
-          <button
-            {...props}
-            className={clsx(
-              "p-2 rounded-3xl",
-              color === "primary" && "bg-teal-500"
-            )}
-            >
-              {children}
-          </button>
-        );
-      case "outlined":
-        return (
-          <button {...props} className="p-2 border-2 border-teal-500 rounded-3xl">{children}</button>
-        );
-      case "text":
-        return <button {...props}>{children}</button>;
-      default:
-        return <button {...props}>{children}</button>;
-    }
-  })()
+  return (
+    <button
+      {...props}
+      className={clsx(
+        "rounded-2xl", variants[variant][color], sizes[size]
+      )}
+    >
+      {children}
+    </button>
+  );
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,7 +5,9 @@ import { Button } from "../base/button"
 const Header: React.FC = () => {
   return (
     <header>
-      <Button variant="contained" color="primary">Jelato</Button>
+      <Button variant="contained" color="secondary" size="medium">Jelato</Button>
+      <Button variant="outlined" color="primary" size="small">Jelato</Button>
+      <Button variant="text" color="secondary" size="medium">Jelato</Button>
     </header>
   )
 }
